e70a6b054973bb3caedaf33dccf78412563758ec
User <krxxl@mail.ru>
2019-12-03 17:31:06 +0400

    Учебный проект: революция или эволюция? (Часть 1)

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,5 +1,5 @@
 import {Colors, Days, MonthNames} from '../const.js';
-import {formatTime} from '../utils.js';
+import {createElement, formatTime} from '../utils.js';
 
 const createColorsMarkup = (colors, currentColor) => {
   return colors
@@ -64,7 +64,7 @@ const createHashtags = (tags) => {
     }).join(`\n`);
 };
 
-export const createTaskEditTemplate = (task) => {
+const createTaskEditTemplate = (task) => {
   const {description, tags, dueDate, color, repeatingDays} = task;
   const isExpired = dueDate instanceof Date && dueDate < Date.now();
   const isDateShowing = !!dueDate;
@@ -163,3 +163,26 @@ export const createTaskEditTemplate = (task) => {
           </article>`
   );
 };
+
+export default class TaskEdit {
+  constructor(task) {
+    this._task = task;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskEditTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
